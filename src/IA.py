c5e0c26dd32e9afb2a04eb454e08b7663f473d6b
Thanar <Thankor@gmail.com>
2010-09-01 11:03:34 +0200

    Primera version semi funcional de IA Signed-off-by: Thanar <Thankor@gmail.com>

--- src/IA.py
+++ src/IA.py
@@ -6,3 +6,36 @@ class IA:
         self.dificultad=1
 
 
+    def update(self,unkown,task):
+        if self.pelotaporencima():
+            print "---------------------------------arriba"
+            self.arriba()
+        elif self.pelotaporabajo():
+            print "---------------------------------abajo"
+            self.abajo()
+        else:
+            self.parado()
+        return task.cont
+
+
+    def abajo(self):
+        self.juego.arriba2false()
+        self.juego.abajo2true()
+
+    def arriba(self):
+        self.juego.abajo2false()
+        self.juego.arriba2true()
+
+    def parado(self):
+        self.juego.arriba2false()
+        self.juego.abajo2false()
+
+    def pelotaporencima(self):
+        print self.juego.pelota.modelo.getZ()-21
+        print self.juego.player2.modelo.getX()-170
+        return self.juego.pelota.modelo.getZ()-21>self.juego.player2.modelo.getX()-160
+
+    def pelotaporabajo(self):
+        return self.juego.pelota.modelo.getZ()-21<self.juego.player2.modelo.getX()-160
+
+
--- src/script.py
+++ src/script.py
@@ -2,6 +2,7 @@
 from pandac.PandaModules import *
 from Jugador import *
 from Pelota import *
+from IA import *
 from ManejadorDeColisiones import *
 from direct.showbase.ShowBase import ShowBase
 
@@ -33,20 +34,9 @@ class MyApp(ShowBase):
 		self.environ.setScale(0.25, 0.25, 0.25)
 		self.environ.setPos(-8, 42, 0)
 		"""
-		# Load and transform the panda actor.
 
-                self.p1 = 1
-                self.p2 = 1
-                self.px = 1
-                self.py = 0
-                self.pz = 20
-                self.i=5
-                self.a=0
-                self.abajo1 = False
-                self.arriba1 = False
-                self.abajo2 = False
-                self.arriba2 = False
-                self.versusIA=False
+		# inicializa variables
+                self.init_variables()
 
                 
                 #carga los jugadores
@@ -111,52 +101,68 @@ class MyApp(ShowBase):
 		polvo.setPos(3.000, 0.000, 2.250)
 		"""
 	
-		self.accept('w',arriba1true,[self])
-		self.accept('w-up',arriba1false,[self])
-		self.accept('s',abajo1true,[self])
-		self.accept('s-up',abajo1false,[self])
+		self.accept('w',self.arriba1true)
+		self.accept('w-up',self.arriba1false)
+		self.accept('s',self.abajo1true)
+		self.accept('s-up',self.abajo1false)
                 #inicia la IA o habilita el control del segundo jugador segun la variable "versusIA"
                 if(self.versusIA==False):
-                    self.accept('o',arriba2true,[self])
-                    self.accept('o-up',arriba2false,[self])
-                    self.accept('l',abajo2true,[self])
-                    self.accept('l-up',abajo2false,[self])
+                    self.accept('o',self.arriba2true)
+                    self.accept('o-up',self.arriba2false)
+                    self.accept('l',self.abajo2true)
+                    self.accept('l-up',self.abajo2false)
                 else:
-                    self.Ia=IA()
+                    self.Ia=IA(self)
                     self.taskMgr.add(self.Ia.update,'IA', extraArgs=[self], appendTask=True)
 
 		#self.taskMgr.add(player1c,'control del jugador 1', extraArgs=[self], appendTask=True)
 		#self.taskMgr.add(player2c,'control del jugador 2', extraArgs=[self], appendTask=True)
-		
 
-def pelotac(self,task):
-	self.pelota.modelo.setPos(self.px,self.py,self.pz)
-	self.px = self.px + 1
-	if self.px > 100:
-		self.px = -100
 
-        #self.pelota.setR(self.a)
-        #self.a+=50
-        #if self.a>360:
-        #    self.a=0
-	#self.i+=0.1
-	return task.cont
-def arriba1true(self):
-	self.player1.arriba= True
-def arriba1false(self):
-	self.player1.arriba= False
-def abajo1true(self):
-	self.player1.abajo= True
-def abajo1false(self):
-	self.player1.abajo= False
-def arriba2true(self):
-	self.player2.arriba= True
-def arriba2false(self):
-	self.player2.arriba= False
-def abajo2true(self):
-	self.player2.abajo= True
-def abajo2false(self):
-	self.player2.abajo= False
+
+	def init_variables(self):
+            #inicializa variables
+            self.p1 = 1
+            self.p2 = 1
+            self.px = 1
+            self.py = 0
+            self.pz = 20
+            self.i=5
+            self.a=0
+            self.abajo1 = False
+            self.arriba1 = False
+            self.abajo2 = False
+            self.arriba2 = False
+            self.versusIA=False
+
+        def pelotac(self,task):
+                self.pelota.modelo.setPos(self.px,self.py,self.pz)
+                self.px = self.px + 1
+                if self.px > 100:
+                        self.px = -100
+
+                #self.pelota.setR(self.a)
+                #self.a+=50
+                #if self.a>360:
+                #    self.a=0
+                #self.i+=0.1
+                return task.cont
+        def arriba1true(self):
+                self.player1.arriba= True
+        def arriba1false(self):
+                self.player1.arriba= False
+        def abajo1true(self):
+                self.player1.abajo= True
+        def abajo1false(self):
+                self.player1.abajo= False
+        def arriba2true(self):
+                self.player2.arriba= True
+        def arriba2false(self):
+                self.player2.arriba= False
+        def abajo2true(self):
+                self.player2.abajo= True
+        def abajo2false(self):
+                self.player2.abajo= False
 
  
 app = MyApp()
